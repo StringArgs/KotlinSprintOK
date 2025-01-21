f34de6ce3f2a567308b81f26bc303fd3d44174e1
StringArgs <oleksiikhomenko17@gmail.com>
2025-01-21 17:47:17 -0500

    KS-2-1 new task refactoring

--- src/main/kotlin/lesson2/task1.kt
+++ src/main/kotlin/lesson2/task1.kt
@@ -5,10 +5,7 @@ fun main(){
     val person2Score = 4
     val person3Score = 3
     val person4Score = 5
-    val allScores = listOf(person1Score, person2Score, person3Score, person4Score)
+    val allScores = ((person1Score + person2Score + person3Score + person4Score).toDouble() / 4)
 
-    println("%.2f".format(allScores.average()))
-
-
-
+    println("%.2f".format(allScores))
 }
