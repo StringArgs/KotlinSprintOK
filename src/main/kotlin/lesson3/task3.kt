c29fb02b798e500a8a9260a4256e59800090dd2e
StringArgs <oleksiikhomenko17@gmail.com>
2025-01-26 11:42:02 -0500

    KS-3-3 task completing

--- src/main/kotlin/lesson3/task3.kt
+++ src/main/kotlin/lesson3/task3.kt
@@ -1,9 +1,9 @@
 package org.example.lesson3
 
 fun main(){
-    val multiplicationTable: Int = readln().toInt()
-    val multiplicationTable1 = listOf(1,2,3,4,5,6,7,8,9)
-    for(i in multiplicationTable1){
-        println("$multiplicationTable x $i = ${multiplicationTable * i}")
+    val multiplicand: Int = readln().toInt()
+    val multiplier = listOf(1,2,3,4,5,6,7,8,9)
+    for(i in multiplier){
+        println("$multiplicand x $i = ${multiplicand * i}")
     }
 }
