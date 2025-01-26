9fe12dc44fdb5d79ddabdd941eea3aca7cd7e32b
StringArgs <oleksiikhomenko17@gmail.com>
2025-01-26 10:47:03 -0500

    KS-2-3 task refactoring. Final commit

--- src/main/kotlin/lesson2/task3.kt
+++ src/main/kotlin/lesson2/task3.kt
@@ -4,7 +4,7 @@ fun main(){
     val departureTimeHours = 9
     val departureTimeMinutes = 39
     var tripTimeMinutes = 457
-    val tripTimeHours = 457/ SAXAGESIMAL_VALUE
+    val tripTimeHours = 457 / SAXAGESIMAL_VALUE
     tripTimeMinutes %= SAXAGESIMAL_VALUE
     var arrivalMinutes = departureTimeMinutes + tripTimeMinutes
     val arrivalHours = departureTimeHours + tripTimeHours + arrivalMinutes / SAXAGESIMAL_VALUE
