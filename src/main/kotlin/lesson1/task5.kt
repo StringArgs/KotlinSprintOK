34af2194a3e1fa001b99377752530446fac92997
StringArgs <oleksiikhomenko17@gmail.com>
2025-01-26 10:45:00 -0500

    KS-1-5 task refactoring. magic number deleted

--- src/main/kotlin/lesson1/task5.kt
+++ src/main/kotlin/lesson1/task5.kt
@@ -2,10 +2,12 @@ package org.example.lesson1
 
 fun main(){
     val seconds = 6480
-    val hours = seconds / 3600
-    val remainSecondsAfterHours = seconds % 3600
-    val minutes = remainSecondsAfterHours / 60
-    val remainSeconds = remainSecondsAfterHours % 60
+    val hours = seconds / (SAXAGESIMAL_VALUE * SAXAGESIMAL_VALUE)
+    val remainSecondsAfterHours = seconds % (SAXAGESIMAL_VALUE * SAXAGESIMAL_VALUE)
+    val minutes = remainSecondsAfterHours / SAXAGESIMAL_VALUE
+    val remainSeconds = remainSecondsAfterHours % SAXAGESIMAL_VALUE
 
     println("%02d:%02d:%02d".format(hours, minutes, remainSeconds))
 }
+
+const val SAXAGESIMAL_VALUE = 60
