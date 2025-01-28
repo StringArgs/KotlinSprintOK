6e729928121dad0f78744d2541669b0f506f4924
StringArgs <oleksiikhomenko17@gmail.com>
2025-01-28 17:59:37 -0500

    KS-4-2 task2 refactoring

--- src/main/kotlin/lesson4/task2.kt
+++ src/main/kotlin/lesson4/task2.kt
@@ -7,15 +7,13 @@ fun main(){
     val cargo2Volume = 80
     val cargo3Weight = 0
     val cargo3Volume = 100
+    val isAverage = cargo1Weight > AVERAGE_MIN_WEIGHT && cargo1Weight <= AVERAGE_MAX_WEIGHT && cargo1Volume < AVERAGE_MAX_VOLUME
 
-    println("Груз с весом $cargo1Weight кг и объемом $cargo1Volume л соответствует категории" +
-            " 'Average': ${cargo1Weight > AVERAGE_CATEGORY_MIN_WEIGHT && cargo1Weight <= AVERAGE_CATEGORY_MAX_WEIGHT && cargo1Volume < AVERAGE_CATEGORY_MAX_VOLUME}")
-    println("Груз с весом $cargo2Weight кг и объемом $cargo2Volume л соответствует категории" +
-            " 'Average': ${cargo2Weight > AVERAGE_CATEGORY_MIN_WEIGHT && cargo2Weight <= AVERAGE_CATEGORY_MAX_WEIGHT && cargo2Volume < AVERAGE_CATEGORY_MAX_VOLUME}")
-    println("Груз с весом $cargo3Weight кг и объемом $cargo3Volume л соответствует категории" +
-            " 'Average': ${cargo3Weight > AVERAGE_CATEGORY_MIN_WEIGHT && cargo1Weight <= AVERAGE_CATEGORY_MAX_WEIGHT && cargo3Volume < AVERAGE_CATEGORY_MAX_VOLUME}")
+    println("Груз с весом $cargo1Weight кг и объемом $cargo1Volume л соответствует категории 'Average': $isAverage")
+    println("Груз с весом $cargo2Weight кг и объемом $cargo2Volume л соответствует категории 'Average': $isAverage")
+    println("Груз с весом $cargo3Weight кг и объемом $cargo3Volume л соответствует категории 'Average': $isAverage")
 }
 
-const val AVERAGE_CATEGORY_MAX_WEIGHT = 100
-const val AVERAGE_CATEGORY_MIN_WEIGHT = 35
-const val AVERAGE_CATEGORY_MAX_VOLUME = 100
+const val AVERAGE_MAX_WEIGHT = 100
+const val AVERAGE_MIN_WEIGHT = 35
+const val AVERAGE_MAX_VOLUME = 100
